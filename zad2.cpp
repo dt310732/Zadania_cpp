a68759a7ea1c550548029f56f476fa6b832a8b3b
dt310732 <dt31072@zpsb.pl>
2026-03-08 09:39:23 +0100

    Summary

---
+++ zad2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+	float a;
+	float b;
+	cout << "Podaj liczbe a:";
+    cin >> a;
+    cout << "Podaj liczbe b:";
+    cin >> b;
+    cout << "Suma: " << a+b << endl;
+    cout << "Roznica: " << a-b << endl;
+    cout << "Iloczyn: " << a*b << endl;
+    return 0;
+}
